fa66f369710b583ac9afad0dce53854c4826c306
RafaelLizardo <rafalpborges@gmail.com>
2024-06-16 20:43:44 -0300

    Atualização do código

--- app/app.js
+++ app/app.js
@@ -43,6 +43,11 @@ const db = new sqlite3.Database('./database.db', (err) => {
 // Configurar o middleware body-parser
 app.use(bodyParser.json());
 
+// Rota para a raiz do servidor
+app.get('/', (req, res) => {
+  res.send('Bem-vindo ao sistema de gestão de livros e empréstimos!');
+});
+
 // Função para criar conta de bibliotecário
 app.post('/bibliotecarios', (req, res) => {
   const { nome, cpf, email, usuario, senha } = req.body;
@@ -59,13 +64,18 @@ app.post('/bibliotecarios', (req, res) => {
 // Função para fazer login de bibliotecário
 app.post('/login', (req, res) => {
   const { email, senha } = req.body;
+  console.log('Email recebido:', email); // Adicionar log para verificar o email recebido
+  console.log('Senha recebida:', senha); // Adicionar log para verificar a senha recebida
+
   db.get('SELECT * FROM Bibliotecario WHERE Email = ? AND Senha = ?', [email, senha], (err, row) => {
     if (err) {
       console.error(err.message);
       res.status(500).send('Erro ao fazer login');
     } else if (!row) {
+      console.log('Credenciais inválidas para email:', email); // Adicionar log para verificar as credenciais inválidas
       res.status(401).send('Credenciais inválidas');
     } else {
+      console.log('Login bem-sucedido para email:', email); // Adicionar log para verificar o login bem-sucedido
       res.status(200).json(row);
     }
   });
@@ -85,10 +95,7 @@ app.post('/alunos', (req, res) => {
 });
 
 
-// Rota para a raiz do servidor
-app.get('/', (req, res) => {
-    res.send('Bem-vindo ao sistema de gestão de livros e empréstimos!');
-  });
+
 
 // Função para consultar aluno por CPF
 app.get('/alunos/:cpf', (req, res) => {
@@ -107,18 +114,34 @@ app.get('/alunos/:cpf', (req, res) => {
 
 app.get('/bibliotecarios/:cpf', (req, res) => {
   const { cpf } = req.params;
+  console.log('Consultando CPF:', cpf); // Adicione este log para verificar o CPF recebido
+
   db.get('SELECT * FROM Bibliotecario WHERE CPFBibliotecario = ?', [cpf], (err, row) => {
     if (err) {
       console.error(err.message);
       res.status(500).send('Erro ao consultar bibliotecário');
     } else if (!row) {
+      console.log('Bibliotecário não encontrado para CPF:', cpf); // Adicione este log para verificar se o CPF não foi encontrado
       res.status(404).send('Bibliotecário não encontrado');
     } else {
+      console.log('Bibliotecário encontrado:', row); // Adicione este log para verificar o resultado da consulta
       res.status(200).json(row);
     }
   });
 });
 
+// Função para cadastrar livro
+app.post('/livros', (req, res) => {
+  const { codigoLivro, nome, autor, editora, ano, categoria, imagemLivro, dataReserva, dataDevolucao, status, cpfAluno } = req.body;
+  db.run('INSERT INTO Livro (CodigoLivro, NomeLivro, Autor, Editora, Ano, Categoria, ImagemLivro, DataReserva, DataDevolucao, Status, CPFAluno) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [codigoLivro, nome, autor, editora, ano, categoria, imagemLivro, dataReserva, dataDevolucao, status, cpfAluno], (err) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Erro ao cadastrar livro');
+    } else {
+      res.status(201).send('Livro cadastrado com sucesso');
+    }
+  });
+});
 
 // Função para pesquisar livro pelo código
 app.get('/livros/:codigo', (req, res) => {
@@ -135,21 +158,10 @@ app.get('/livros/:codigo', (req, res) => {
   });
 });
 
-// Função para cadastrar livro
-app.post('/livros', (req, res) => {
-  const { nome, autor, editora, ano, categoria, imagemLivro, dataReserva, dataDevolucao, status, cpfAluno } = req.body;
-  db.run('INSERT INTO Livro (NomeLivro, Autor, Editora, Ano, Categoria, ImagemLivro, DataReserva, DataDevolucao, Status, CPFAluno) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [nome, autor, editora, ano, categoria, imagemLivro, dataReserva, dataDevolucao, status, cpfAluno], (err) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Erro ao cadastrar livro');
-    } else {
-      res.status(201).send('Livro cadastrado com sucesso');
-    }
-  });
-});
+
 
 // Função para atualizar cadastro de livro
-app.put('/livros/:codigo', (req, res) => {
+app.put('/livros/atualizar/:codigo', (req, res) => {
   const { nome, autor, editora, ano, categoria, imagemLivro, dataReserva, dataDevolucao, status, cpfAluno } = req.body;
   const { codigo } = req.params;
   db.run('UPDATE Livro SET NomeLivro = ?, Autor = ?, Editora = ?, Ano = ?, Categoria = ?, ImagemLivro = ?, DataReserva = ?, DataDevolucao = ?, Status = ?, CPFAluno = ? WHERE CodigoLivro = ?', [nome, autor, editora, ano, categoria, imagemLivro, dataReserva, dataDevolucao, status, cpfAluno, codigo], (err) => {
@@ -163,7 +175,7 @@ app.put('/livros/:codigo', (req, res) => {
 });
 
 // Função para apagar cadastro de livro
-app.delete('/livros/:codigo', (req, res) => {
+app.delete('/livros/delete/:codigo', (req, res) => {
   const { codigo } = req.params;
   db.run('DELETE FROM Livro WHERE CodigoLivro = ?', [codigo], (err) => {
     if (err) {
@@ -184,6 +196,62 @@ app.get('/pendencias', (req, res) => {
     } else {
       res.status(200).json(rows);
     }
+  });
+});
+
+app.post('/devolver/:codigoLivro', (req, res) => {
+  const { codigoLivro } = req.params;
+
+  // Verificar se o livro está reservado
+  db.get('SELECT * FROM Livro WHERE CodigoLivro = ?', [codigoLivro], (err, livro) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Erro ao verificar reserva do livro');
+      return;
+    }
+
+    if (livro.Status !== 'Reservado') {
+      res.status(400).send('Livro não está reservado');
+      return;
+    }
+
+    // Limpar informações de reserva do livro
+    db.run('UPDATE Livro SET Status = ?, CPFAluno = ? WHERE CodigoLivro = ?', ['Disponível', null, codigoLivro], (err) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send('Erro ao devolver livro');
+      } else {
+        res.status(200).send('Livro devolvido com sucesso');
+      }
+    });
+  });
+});
+
+app.post('/reservar/:codigoLivro/:cpfAluno', (req, res) => {
+  const { codigoLivro, cpfAluno } = req.params;
+
+  // Verificar se o livro já está reservado
+  db.get('SELECT * FROM Livro WHERE CodigoLivro = ?', [codigoLivro], (err, livro) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Erro ao verificar reserva do livro');
+      return;
+    }
+
+    if (livro.Status === 'Reservado') {
+      res.status(400).send('Livro já está reservado');
+      return;
+    }
+
+    // Atualizar status do livro para reservado e adicionar CPF do aluno
+    db.run('UPDATE Livro SET Status = ?, CPFAluno = ? WHERE CodigoLivro = ?', ['Reservado', cpfAluno, codigoLivro], (err) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send('Erro ao reservar livro');
+      } else {
+        res.status(200).send('Livro reservado com sucesso');
+      }
+    });
   });
 });
 
